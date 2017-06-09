79702c9fb091daffd6eff977203b3e825bfbc5c0
nirgedeng <43745594@qq.com>
2017-06-10 00:20:13 +0800

    网络

---
+++ Nirge.Core/TcpClient.cs
@@ -0,0 +1,19 @@
+﻿/*------------------------------------------------------------------
+    Copyright ? : All rights reserved
+    Author      : 邓晓峰
+------------------------------------------------------------------*/
+
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System;
+
+namespace Nirge.Core
+{
+    public class CTcpClient
+    {
+    }
+}
