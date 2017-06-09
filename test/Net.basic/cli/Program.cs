858c139a909e770aeee0b70c576c0f4f1c567524
nirgedeng <43745594@qq.com>
2017-06-09 22:53:49 +0800

    网络测试工程提交

---
+++ test/Net.basic/cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
