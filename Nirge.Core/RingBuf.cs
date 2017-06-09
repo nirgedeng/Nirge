f78803717a5cc7545e5ce3863192ea0b5d6c342d
nirgedeng <43745594@qq.com>
2017-06-10 00:00:38 +0800

    循环缓冲区实现

---
+++ Nirge.Core/RingBuf.cs
@@ -0,0 +1,147 @@
+﻿/*------------------------------------------------------------------
+    Copyright ? : All rights reserved
+    Author      : 邓晓峰
+------------------------------------------------------------------*/
+
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System;
+
+namespace Nirge.Core
+{
+    public class CRingBuf
+    {
+        byte[] _buf;
+        int _head;
+        int _tail;
+        int _used;
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return _used == 0;
+            }
+        }
+
+        public bool IsFull
+        {
+            get
+            {
+                return _used == _buf.Length;
+            }
+        }
+
+        public int UsedCapacity
+        {
+            get
+            {
+                return _used;
+            }
+        }
+
+        public int UnusedCapacity
+        {
+            get
+            {
+                return _buf.Length - _used;
+            }
+        }
+
+        public CRingBuf(int capacity)
+        {
+            _buf = new byte[capacity];
+            Clear();
+        }
+
+        public void Clear()
+        {
+            _head = 0;
+            _tail = 0;
+            _used = 0;
+        }
+
+        public bool Write(byte[] buf, int offset, int count)
+        {
+            if (buf == null)
+                return false;
+            if (count == 0)
+                return false;
+            if (UnusedCapacity < count)
+                return false;
+
+            if (_head < _tail)
+            {
+                Buffer.BlockCopy(buf, offset, _buf, _head, count);
+                _head += count;
+            }
+            else
+            {
+                var p = _buf.Length - _head;
+                if (p < count)
+                {
+                    var q = count - p;
+                    Buffer.BlockCopy(buf, offset, _buf, _head, p);
+                    Buffer.BlockCopy(buf, offset + p, _buf, 0, q);
+                    _head = q;
+                }
+                else
+                {
+                    Buffer.BlockCopy(buf, offset, _buf, _head, count);
+                    _head += count;
+                }
+            }
+
+            _used += count;
+
+            return true;
+        }
+
+        public bool Read(int count, out byte[] buf)
+        {
+            if (count == 0)
+            {
+                buf = null;
+                return false;
+            }
+
+            if (count < UsedCapacity)
+            {
+                buf = null;
+                return false;
+            }
+
+            buf = new byte[count];
+
+            if (_head > _tail)
+            {
+                Buffer.BlockCopy(_buf, _tail, buf, 0, count);
+                _tail += count;
+            }
+            else
+            {
+                var p = _buf.Length - _tail;
+                if (p < count)
+                {
+                    var q = count - p;
+                    Buffer.BlockCopy(_buf, _tail, buf, 0, p);
+                    Buffer.BlockCopy(_buf, 0, buf, p, q);
+                    _tail = q;
+                }
+                else
+                {
+                    Buffer.BlockCopy(_buf, _tail, buf, 0, count);
+                    _tail += count;
+                }
+            }
+
+            _used -= count;
+
+            return true;
+        }
+    }
+}
