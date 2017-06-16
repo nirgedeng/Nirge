34cdb448a2b29f55e10f6c54b08c1d383fec6162
nirgedeng <43745594@qq.com>
2017-06-16 22:24:36 +0800

    1

---
+++ test/Net.basic/cli/Cli1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using Nirge.Core;
+
+namespace cli
+{
+    public class Cli1
+    {
+        CTcpClient _cli;
+        List<byte[]> _pkgs;
+        int _batch;
+
+        public Cli1()
+        {
+            _pkgs = new List<byte[]>();
+
+            for (int i = 1; i <= 200; ++i)
+            {
+                var pkg = new byte[i];
+                pkg[0] = (byte)i;
+                _pkgs.Add(pkg);
+            }
+        }
+
+        public void Connect(IPEndPoint addr)
+        {
+            _cli = new CTcpClient(new CTcpClientArgs()
+            {
+                SendBufferSize = 8192,
+                ReceiveBufferSize = 8192,
+                SendQueueSize = 1024,
+                RecvQueueSize = 1024,
+                Log = null,
+            });
+
+            _cli.Connected += OnConnected;
+            _cli.Closed += OnClosed;
+            _cli.Recved += OnRecvd;
+
+            _cli.Connect(addr);
+        }
+
+        void OnConnected(object sender, CDataEventArgs<CTcpClientConnectArgs> e)
+        {
+            Console.WriteLine("OnConnect {0}:{1}:{2}", e.Arg1.Result, e.Arg1.Error, e.Arg1.SocketError);
+        }
+
+        void OnClosed(object sender, CDataEventArgs<CTcpClientCloseArgs> e)
+        {
+            Console.WriteLine("OnClosed {0}:{1}:{2}", e.Arg1.Reason, e.Arg1.Error, e.Arg1.SocketError);
+        }
+
+        void OnRecvd(byte[] arg1, int arg2, int arg3)
+        {
+        }
+
+        public void Exec()
+        {
+            if (_cli == null)
+                return;
+
+            _cli.Exec();
+
+            for (int i = _batch * 20; i < (_batch + 1) * 20; ++i)
+            {
+                _cli.Send(_pkgs[i], 0, _pkgs[i].Length);
+            }
+
+            _batch = ++_batch % 10;
+        }
+
+    }
+}
---
+++ test/Net.basic/ser/Cli1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nirge.Core;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ser
+{
+    public class Cli1
+    {
+        public int _cli;
+
+        public Cli1(int cli)
+        {
+            _cli = cli;
+        }
+
+        public void OnConnected()
+        {
+            Console.WriteLine("OnConnected, {0}", _cli);
+        }
+
+        public void OnClosed(CTcpClientCloseArgs e)
+        {
+            Console.WriteLine("OnClosed, {0},{1},{2},{3}", _cli, e.Reason, e.Error, e.SocketError);
+        }
+
+        public void OnRecved(byte[] buf, int offset, int count)
+        {
+            Console.WriteLine("OnRecved, {0},{1},{2}", _cli, count, buf[0]);
+        }
+    }
+}
