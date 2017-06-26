a1c9511fdfb27208f7bae49b870706edca057eff
nirgedeng <43745594@qq.com>
2017-06-26 21:03:10 +0800

    1

--- test/Net.task/ser/Program.cs
+++ test/Net.task/ser/Program.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿/*------------------------------------------------------------------
+    Copyright © : All rights reserved
+    Author      : 邓晓峰
+------------------------------------------------------------------*/
+
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using log4net;
 using Nirge.Core;
 using System.Net;
+using log4net;
+using System;
 
 namespace ser
 {
