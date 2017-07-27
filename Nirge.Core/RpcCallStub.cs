f4fe3d935248064847e0a51b7097b90210c7b7bc
nirgedeng <43745594@qq.com>
2017-07-27 21:00:57 +0800

    1

---
+++ Nirge.Core/RpcCallStub.cs
@@ -0,0 +1,19 @@
+﻿/*------------------------------------------------------------------
+    Copyright © : All rights reserved
+    Author      : 邓晓峰
+------------------------------------------------------------------*/
+
+using System.Reflection;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.IO;
+using log4net;
+using System;
+
+namespace Nirge.Core
+{
+    public class RpcCallStub
+    {
+    }
+}
