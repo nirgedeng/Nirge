4288f0debd307b2181b4a87bf45d897701212164
nirgedeng <43745594@qq.com>
2017-07-29 22:19:43 +0800

    1

---
+++ Nirge.Core/Task/TaskTimer.cs
@@ -0,0 +1,134 @@
+﻿/*------------------------------------------------------------------
+    Copyright © : All rights reserved
+    Author      : 邓晓峰
+------------------------------------------------------------------*/
+
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.IO;
+using log4net;
+using System;
+
+namespace Nirge.Core
+{
+    public class CTaskTimer
+    {
+        const int gIntervalMin = 10;
+
+        struct CTimer
+        {
+            public ITask _task;
+            public int _interval;
+            public int _pass;
+            public int _count;
+        }
+
+        CTasker _proc;
+        ILog _log;
+        int _timerId;
+        Dictionary<int, CTimer> _timers;
+        List<int> _timersAfter;
+
+        public CTaskTimer(CTasker proc, ILog log)
+        {
+            _proc = proc;
+            _log = log;
+
+            _timerId = 0;
+            _timers = new Dictionary<int, CTimer>(32);
+            _timersAfter = new List<int>(32);
+        }
+
+        public void Init()
+        {
+        }
+
+        public void Destroy()
+        {
+            Clear();
+        }
+
+        public int Reg(ITask task, int interval, int count = -1)
+        {
+            if (task == null)
+                return -1;
+
+            if (interval < gIntervalMin)
+                interval = gIntervalMin;
+
+            var timerId = ++_timerId;
+            _timers.Add(timerId, new CTimer()
+            {
+                _task = task,
+                _interval = interval,
+                _pass = 0,
+                _count = count,
+            });
+
+            return timerId;
+        }
+
+        public bool Unreg(int timer)
+        {
+            return _timers.Remove(timer);
+        }
+
+        public void Clear()
+        {
+            _timerId = 0;
+            _timers.Clear();
+            _timersAfter.Clear();
+        }
+
+        public void Exec(int tick)
+        {
+            Trace.Assert(tick > 0 && tick <= gIntervalMin, string.Format("{0} > 0 && {0} <= gIntervalMin", tick));
+            eExec(tick);
+        }
+
+        void eExec(int tick)
+        {
+            if (_timers.Count == 0)
+                return;
+
+            _timersAfter.AddRange(_timers.Keys);
+
+            foreach (var i in _timersAfter)
+            {
+                CTimer timer;
+                if (_timers.TryGetValue(i, out timer))
+                    eExec(tick, i, timer);
+            }
+
+            _timersAfter.Clear();
+        }
+
+        void eExec(int tick, int timerId, CTimer timer)
+        {
+            timer._pass += tick;
+            if (timer._pass < timer._interval)
+                return;
+            timer._pass = 0;
+
+            if (timer._count > 0)
+                --timer._count;
+
+            try
+            {
+                timer._task.Exec();
+            }
+            catch (Exception exception)
+            {
+                _log.Error(string.Format("[Timer]Exec exception, timer:\"{0}\", task:\"{1}\"", timerId, timer._task.GetType()), exception);
+            }
+
+            if (timer._count == 0)
+                _timers.Remove(timerId);
+        }
+    }
+}
---
+++ Nirge.Core/Task/Tasker.cs
@@ -0,0 +1,195 @@
+﻿/*------------------------------------------------------------------
+    Copyright ? : All rights reserved
+    Author      : 邓晓峰
+------------------------------------------------------------------*/
+
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.IO;
+using log4net;
+using System;
+
+namespace Nirge.Core
+{
+    #region
+
+    public class CTaskerArgs
+    {
+        int _procs;
+        int _taskCapacity;
+
+        public int Procs
+        {
+            get
+            {
+                return _procs;
+            }
+        }
+
+        public int TaskCapacity
+        {
+            get
+            {
+                return _taskCapacity;
+            }
+        }
+
+        public CTaskerArgs(int procs = 0, int taskCapacity = 0)
+        {
+            _procs = procs;
+            _taskCapacity = taskCapacity;
+
+            if (_procs == 0)
+                _procs = 1;
+            else if (_procs < 1)
+                _procs = 1;
+            else if (_procs > Environment.ProcessorCount)
+                _procs = Environment.ProcessorCount;
+            _taskCapacity = 1024;
+        }
+    }
+
+    #endregion
+
+    public class CTasker
+    {
+        CTaskerArgs _args;
+        ILog _log;
+        List<Thread> _procs;
+        Queue<ITask> _tasks;
+        Queue<ITask> _tasksAfter;
+        bool _quit;
+
+        public int TasksCount
+        {
+            get
+            {
+                return _tasks.Count + _tasksAfter.Count;
+            }
+        }
+
+        CTasker(CTaskerArgs args, ILog log)
+        {
+            _args = args;
+            _log = log;
+
+            _procs = new List<Thread>(_args.Procs);
+            for (int i = 0, len = _args.Procs; i < len; ++i)
+            {
+                var proc = new Thread(Exec);
+                proc.IsBackground = true;
+                _procs.Add(proc);
+            }
+
+            _tasks = new Queue<ITask>(_args.TaskCapacity);
+            _tasksAfter = new Queue<ITask>(_args.TaskCapacity);
+
+            _quit = false;
+        }
+        public CTasker(ILog log)
+            :
+            this(new CTaskerArgs(), log)
+        {
+        }
+
+        public void Init()
+        {
+            foreach (var i in _procs)
+                i.Start();
+        }
+
+        public void Destroy()
+        {
+            Close();
+        }
+
+        void Close()
+        {
+            _quit = true;
+
+            lock (_tasks)
+            {
+                Monitor.Pulse(_tasks);
+            }
+
+            foreach (var i in _procs)
+                i.Join();
+
+            _procs.Clear();
+        }
+
+        public void Clear()
+        {
+            lock (_tasks)
+            {
+                _tasks.Clear();
+            }
+        }
+
+        public void Exec(IEnumerable<ITask> tasks)
+        {
+            if (_quit)
+                return;
+
+            lock (_tasks)
+            {
+                foreach (var i in tasks)
+                    _tasks.Enqueue(i);
+                Monitor.Pulse(_tasks);
+            }
+        }
+        public void Exec(ITask task)
+        {
+            if (_quit)
+                return;
+
+            lock (_tasks)
+            {
+                _tasks.Enqueue(task);
+                Monitor.Pulse(_tasks);
+            }
+        }
+        void Exec()
+        {
+            lock (_tasks)
+            {
+                while (true)
+                {
+                    if (_tasks.Count == 0)
+                    {
+                        if (_quit)
+                            break;
+                        else
+                            Monitor.Wait(_tasks);
+                    }
+                    else
+                    {
+                        while (_tasks.Count > 0)
+                            _tasksAfter.Enqueue(_tasks.Dequeue());
+
+                        Monitor.Exit(_tasks);
+
+                        while (_tasksAfter.Count > 0)
+                        {
+                            var task = _tasksAfter.Dequeue();
+
+                            try
+                            {
+                                task.Exec();
+                            }
+                            catch (Exception exception)
+                            {
+                                _log.Error(string.Format("[Task]Exec exception, type:\"{0}\"", task.GetType()), exception);
+                            }
+                        }
+
+                        Monitor.Enter(_tasks);
+                    }
+                }
+            }
+        }
+    }
+}
