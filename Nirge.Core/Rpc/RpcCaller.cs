ca3a2ce77f5bf824ce710e870d536a2a07cc6b58
nirgedeng <43745594@qq.com>
2017-07-29 14:17:38 +0800

    1

---
+++ Nirge.Core/Rpc/RpcCaller.cs
@@ -0,0 +1,57 @@
+﻿/*------------------------------------------------------------------
+    Copyright © : All rights reserved
+    Author      : 邓晓峰
+------------------------------------------------------------------*/
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.IO;
+using log4net;
+using System;
+
+namespace Nirge.Core
+{
+    public class RpcCaller
+    {
+        ILog _log;
+        RpcCallStubProvider _stubs;
+        RpcChannel _channel;
+
+        public RpcCaller(ILog log, RpcCallStubProvider stubs, RpcChannel channel)
+        {
+            _log = log;
+            _stubs = stubs;
+            _channel = channel;
+        }
+
+        async Task<ArraySegment<byte>> Call(int service, int call, ArraySegment<byte> args)
+        {
+            var stub = _stubs.CreateStub(service, call);
+            var task = stub.Awaiter.Task;
+
+            var pkg = new C2sRpcCall()
+            {
+                Serial = stub.Serial,
+                Service = stub.Service,
+                Call = stub.Call,
+            };
+
+            try
+            {
+                await task;
+            }
+            catch (RpcException exception)
+            {
+            }
+            catch (Exception exception)
+            {
+            }
+
+            return task.Result;
+        }
+    }
+}
---
+++ Nirge.Core/Rpc/RpcUtils.cs
@@ -0,0 +1,87 @@
+﻿/*------------------------------------------------------------------
+    Copyright © : All rights reserved
+    Author      : 邓晓峰
+------------------------------------------------------------------*/
+
+using System.Reflection;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.IO;
+using log4net;
+using System;
+
+namespace Nirge.Core
+{
+    public enum RpcCallException
+    {
+        None,
+        CalleeArgsDeserialize,
+        CalleeExec,
+        CalleeRetSerialize,
+    }
+
+    public class RpcException : Exception
+    {
+        public RpcException() : base() { }
+        public RpcException(string message) : base(message) { }
+        public RpcException(string message, Exception innerException) : base(message, innerException) { }
+    }
+
+    public class CallerChannelRpcException : RpcException
+    {
+        public CallerChannelRpcException()
+        {
+        }
+    }
+
+    public class CallerArgsSerializeRpcException : RpcException
+    {
+        public CallerArgsSerializeRpcException()
+        {
+        }
+    }
+
+    public class CallerRetDeserializeRpcException : RpcException
+    {
+        public CallerRetDeserializeRpcException()
+        {
+        }
+    }
+
+    public class CallerTimeoutRpcException : RpcException
+    {
+        public CallerTimeoutRpcException()
+        {
+        }
+    }
+
+    public class CalleeArgsDeserializeRpcException : RpcException
+    {
+        public CalleeArgsDeserializeRpcException()
+        {
+        }
+    }
+
+    public class CalleeExecRpcException : RpcException
+    {
+        public CalleeExecRpcException()
+        {
+        }
+    }
+
+    public class CalleeRetSerializeRpcException : RpcException
+    {
+        public CalleeRetSerializeRpcException()
+        {
+        }
+    }
+
+    public enum RpcProto
+    {
+        C2sRpcCall = 1,
+
+        S2cRpcCall = 11,
+        S2cRpcCallException,
+    }
+}
