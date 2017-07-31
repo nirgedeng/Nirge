31e3592c8b1c17ff402e86894c499d1e88bad6b7
nirgedeng <43745594@qq.com>
2017-07-31 11:27:58 +0800

    1

--- Nirge.Core/Rpc/RpcUtils.cs
+++ Nirge.Core/Rpc/RpcUtils.cs
@@ -16,78 +16,6 @@ namespace Nirge.Core
 {
     #region 
 
-    public enum eRpcException
-    {
-        None,
-        CallerChannel,
-        CallerArgsSerialize,
-        CallerRetDeserialize,
-        CallerTimeout,
-        CalleeArgsDeserialize,
-        CalleeExec,
-        CalleeRetSerialize,
-    }
-
-    public class CRpcException : Exception
-    {
-        public CRpcException() : base() { }
-        public CRpcException(string message) : base(message) { }
-        public CRpcException(string message, Exception innerException) : base(message, innerException) { }
-    }
-
-    public class CCallerChannelRpcException : CRpcException
-    {
-        public CCallerChannelRpcException()
-        {
-        }
-    }
-
-    public class CCallerArgsSerializeRpcException : CRpcException
-    {
-        public CCallerArgsSerializeRpcException()
-        {
-        }
-    }
-
-    public class CCallerRetDeserializeRpcException : CRpcException
-    {
-        public CCallerRetDeserializeRpcException()
-        {
-        }
-    }
-
-    public class CCallerTimeoutRpcException : CRpcException
-    {
-        public CCallerTimeoutRpcException()
-        {
-        }
-    }
-
-    public class CCalleeArgsDeserializeRpcException : CRpcException
-    {
-        public CCalleeArgsDeserializeRpcException()
-        {
-        }
-    }
-
-    public class CCalleeExecRpcException : CRpcException
-    {
-        public CCalleeExecRpcException()
-        {
-        }
-    }
-
-    public class CCalleeRetSerializeRpcException : CRpcException
-    {
-        public CCalleeRetSerializeRpcException()
-        {
-        }
-    }
-
-    #endregion
-
-    #region 
-
     public abstract class CRpcCommunicator
     {
         public abstract bool Send(int channel, byte[] buf, int offset, int count);
@@ -168,6 +96,78 @@ namespace Nirge.Core
             _inputStream.Dispose();
             _outputStream.Dispose();
             _stream.Dispose();
+        }
+    }
+
+    #endregion
+
+    #region 
+
+    public enum eRpcException
+    {
+        None,
+        CallerChannel,
+        CallerArgsSerialize,
+        CallerRetDeserialize,
+        CallerTimeout,
+        CalleeArgsDeserialize,
+        CalleeExec,
+        CalleeRetSerialize,
+    }
+
+    public class CRpcException : Exception
+    {
+        public CRpcException() : base() { }
+        public CRpcException(string message) : base(message) { }
+        public CRpcException(string message, Exception innerException) : base(message, innerException) { }
+    }
+
+    public class CCallerChannelRpcException : CRpcException
+    {
+        public CCallerChannelRpcException()
+        {
+        }
+    }
+
+    public class CCallerArgsSerializeRpcException : CRpcException
+    {
+        public CCallerArgsSerializeRpcException()
+        {
+        }
+    }
+
+    public class CCallerRetDeserializeRpcException : CRpcException
+    {
+        public CCallerRetDeserializeRpcException()
+        {
+        }
+    }
+
+    public class CCallerTimeoutRpcException : CRpcException
+    {
+        public CCallerTimeoutRpcException()
+        {
+        }
+    }
+
+    public class CCalleeArgsDeserializeRpcException : CRpcException
+    {
+        public CCalleeArgsDeserializeRpcException()
+        {
+        }
+    }
+
+    public class CCalleeExecRpcException : CRpcException
+    {
+        public CCalleeExecRpcException()
+        {
+        }
+    }
+
+    public class CCalleeRetSerializeRpcException : CRpcException
+    {
+        public CCalleeRetSerializeRpcException()
+        {
         }
     }
 
