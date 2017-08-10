d953e78acb0fff02d485bbb873ee41c381035de8
nirgedeng <43745594@qq.com>
2017-08-10 20:19:12 +0800

    1

--- test/Net.rpc/proto/rpc.cs
+++ test/Net.rpc/proto/rpc.cs
@@ -13,16 +13,6 @@ using System;
 
 namespace Nirge.Core
 {
-    public interface IGameRpcService : IRpcService
-    {
-        void f(int channel);
-        void g(int channel, gargs args);
-
-        void h(int channel);
-        void p(int channel, pargs args);
-        qret q(int channel, qargs args);
-    }
-
     public class CGameRpcService : IGameRpcService
     {
         public void f(int channel)
@@ -53,89 +43,6 @@ namespace Nirge.Core
                 B = args.B,
                 C = args.A,
             };
-        }
-
-    }
-
-    public class CGameRpcCaller : CRpcCaller
-    {
-        public CGameRpcCaller(CRpcCallerArgs args, ILog log, CRpcStream stream, CRpcCommunicator communicator, CRpcCallStubProvider stubs)
-            : base(args, log, stream, communicator, stubs, null, 1)
-        {
-        }
-
-        public void f(int channel = 0)
-        {
-            Call<RpcCallArgsEmpty>(channel, 1, ArgsEmpty);
-        }
-
-        public void g(gargs args, int channel = 0)
-        {
-            Call<gargs>(channel, 2, args);
-        }
-
-        public Task<RpcCallArgsEmpty> h(int channel = 0)
-        {
-            return CallAsync<RpcCallArgsEmpty, RpcCallArgsEmpty>(channel, 3, ArgsEmpty);
-        }
-        public Task<RpcCallArgsEmpty> p(pargs args, int channel = 0)
-        {
-            return CallAsync<pargs, RpcCallArgsEmpty>(channel, 4, args);
-        }
-        public Task<qret> q(qargs args, int channel = 0)
-        {
-            return CallAsync<qargs, qret>(channel, 5, args);
-        }
-
-    }
-
-    [CRpcService(1)]
-    public class CGameRpcCallee : CRpcCallee<IGameRpcService>
-    {
-        public CGameRpcCallee(CRpcCalleeArgs args, ILog log, CRpcStream stream, CRpcCommunicator communicator, IGameRpcService service)
-            : base(args, log, stream, communicator, null, service)
-        {
-        }
-
-        public override void Call(int channel, RpcCallReq req)
-        {
-            switch (req.Call)
-            {
-            case 1:
-                Call<RpcCallArgsEmpty, RpcCallArgsEmpty>(channel, req, (_, args) =>
-                {
-                    _service.f(channel);
-                    return ArgsEmpty;
-                });
-                break;
-            case 2:
-                Call<gargs, RpcCallArgsEmpty>(channel, req, (_, args) =>
-                {
-                    _service.g(channel, args);
-                    return ArgsEmpty;
-                });
-                break;
-            case 3:
-                CallAsync<RpcCallArgsEmpty, RpcCallArgsEmpty>(channel, req, (_, args) =>
-                {
-                    _service.h(channel);
-                    return ArgsEmpty;
-                });
-                break;
-            case 4:
-                CallAsync<pargs, RpcCallArgsEmpty>(channel, req, (_, args) =>
-                {
-                    _service.p(channel, args);
-                    return ArgsEmpty;
-                });
-                break;
-            case 5:
-                CallAsync<qargs, qret>(channel, req, _service.q);
-                break;
-            default:
-                base.Call(channel, req);
-                break;
-            }
         }
 
     }
