235882b1e25e69beeb253b0a6d097e682e4a3369
nirgedeng <43745594@qq.com>
2017-08-23 12:25:43 +0800

    1

--- test/Net.multi/cli/Program.cs
+++ test/Net.multi/cli/Program.cs
@@ -52,7 +52,7 @@ namespace cli
                 _cli.Connected += OnConnected;
                 _cli.Closed += OnClosed;
                 _cli.Recved += OnRecvd;
-                _cli.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9527));
+                _cli.Connect(new IPEndPoint(IPAddress.Parse("10.12.236.197"), 9527));
             }
 
             public void Destroy()
