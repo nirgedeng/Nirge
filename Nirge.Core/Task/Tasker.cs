92c380ce39641084350605717bdbe88bb9474fbf
nirgedeng <43745594@qq.com>
2017-08-29 11:02:07 +0800

    1

--- Nirge.Core/Task/Tasker.cs
+++ Nirge.Core/Task/Tasker.cs
@@ -1,8 +1,9 @@
 ﻿/*------------------------------------------------------------------
-    Copyright ? : All rights reserved
+    Copyright © : All rights reserved
     Author      : 邓晓峰
 ------------------------------------------------------------------*/
 
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Threading;
@@ -54,6 +55,7 @@ namespace Nirge.Core
 
     #endregion
 
+#if CW
     public class CTasker
     {
         CTaskerArgs _args;
@@ -193,4 +195,118 @@ namespace Nirge.Core
             }
         }
     }
+#else
+    public class CTasker
+    {
+        CTaskerArgs _args;
+        ILog _log;
+        List<Thread> _procs;
+        ConcurrentQueue<ITask> _tasks;
+        bool _quit;
+
+        public int TasksCount
+        {
+            get
+            {
+                return _tasks.Count;
+            }
+        }
+
+        CTasker(CTaskerArgs args, ILog log)
+        {
+            _args = args;
+            _log = log;
+
+            _procs = new List<Thread>(_args.Procs);
+            for (int i = 0, len = _args.Procs; i < len; ++i)
+            {
+                var proc = new Thread(Exec);
+                proc.IsBackground = true;
+                _procs.Add(proc);
+            }
+
+            _tasks = new ConcurrentQueue<ITask>();
+
+            _quit = false;
+        }
+        public CTasker(ILog log)
+            :
+            this(new CTaskerArgs(), log)
+        {
+        }
+
+        public void Init()
+        {
+            foreach (var i in _procs)
+                i.Start();
+        }
+
+        public void Destroy()
+        {
+            Close();
+        }
+
+        void Close()
+        {
+            _quit = true;
+
+            foreach (var i in _procs)
+                i.Join();
+
+            _procs.Clear();
+        }
+
+        public void Clear()
+        {
+            _tasks.Clear();
+        }
+
+        public void Exec(IEnumerable<ITask> tasks)
+        {
+            if (_quit)
+                return;
+
+            foreach (var i in tasks)
+                _tasks.Enqueue(i);
+        }
+        public void Exec(ITask task)
+        {
+            if (_quit)
+                return;
+
+            _tasks.Enqueue(task);
+        }
+        void Exec()
+        {
+            lock (_tasks)
+            {
+                while (true)
+                {
+                    if (_tasks.TryDequeue(out var task))
+                    {
+                        try
+                        {
+                            task.Exec();
+                        }
+                        catch (Exception exception)
+                        {
+                            _log.Error(string.Format("[Task]Exec exception, type:\"{0}\""
+                                , task.GetType()), exception);
+                        }
+                    }
+                    else
+                    {
+                        if (_tasks.Count == 0)
+                        {
+                            if (_quit)
+                                break;
+                            else
+                                Thread.Sleep(1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+#endif
 }
