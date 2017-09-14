91f09abd12f4273246b400ec9cae20496dfb9143
nirgedeng <43745594@qq.com>
2017-09-14 19:57:17 +0800

    1

--- test/Data/Program.cs
+++ test/Data/Program.cs
@@ -3,8 +3,14 @@
     Author      : 邓晓峰
 ------------------------------------------------------------------*/
 
-using System;
+using Google.Protobuf.Reflection;
+using Google.Protobuf;
+using log4net.Config;
+using OfficeOpenXml;
 using Nirge.Core;
+using System.IO;
+using log4net;
+using System;
 
 namespace Data
 {
@@ -12,6 +18,13 @@ namespace Data
     {
         static void Main(string[] args)
         {
+            XmlConfigurator.Configure(LogManager.CreateRepository("data"), new FileInfo("../../Data.log.xml"));
+
+            using (var xls = new ExcelPackage(File.OpenRead(@"../../a.xlsx")))
+            {
+                var ds = new CDataAsset<a>(LogManager.Exists("data", "all"), a.Descriptor);
+                ds.Load(xls.Workbook.Worksheets[1]);
+            }
         }
     }
 }
