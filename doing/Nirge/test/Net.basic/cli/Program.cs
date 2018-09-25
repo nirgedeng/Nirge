b396f32829d543e10f1a09f21ea0d60fdcab70fb
nirgedeng <43745594@qq.com>
2018-09-25 21:20:56 +0800

    1

---
+++ doing/Nirge/test/Net.basic/cli/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+using System.IO;
+using System.Threading;
+using Nirge.Core;
+using System.Net;
+using System.Collections.Generic;
+
+namespace cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(LogManager.CreateRepository("cli"), new FileInfo("../../Net.basic.log.cli.xml"));
+            var cache = new CTcpClientCache(new CTcpClientCacheArgs());
+
+            var clis = new List<CTcpClient>(1024);
+            for (var i = 0; i < clis.Capacity; ++i)
+            {
+                var cli = new CTcpClient(new CTcpClientArgs(), LogManager.Exists("cli", "all"), cache);
+                clis.Add(cli);
+                cli.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9527));
+            }
+
+            var rng = new Random();
+
+            while (true)
+            {
+                foreach (var i in clis)
+                {
+                    i.Exec();
+                    if (i.State == eTcpClientState.Connected)
+                    {
+                        byte[] buf;
+                        cache.AllocSendBuf(rng.Next(1024), out buf);
+                        i.Send(buf);
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
---
+++ doing/Nirge/test/Net.basic/ser/Program.cs
@@ -0,0 +1,52 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using Nirge.Core;
+using System.Net;
+using System.Threading;
+
+namespace ser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(LogManager.CreateRepository("ser"), new FileInfo("../../Net.basic.log.ser.xml"));
+
+            var ser = new CTcpServer(LogManager.Exists("ser", "all"));
+            ser.Closed += Ser_Closed;
+            ser.CliConnected += Ser_CliConnected;
+            ser.CliClosed += Ser_CliClosed;
+            ser.CliRecved += Ser_CliRecved;
+            ser.Open(new IPEndPoint(IPAddress.Any, 9527));
+
+            while (true)
+            {
+                ser.Exec();
+                Thread.Sleep(1);
+            }
+
+        }
+
+        private static void Ser_CliRecved(object arg1, int arg2, byte[] arg3, int arg4, int arg5)
+        {
+            //Console.WriteLine("cli {0} recv msg {1}", arg2, arg5);
+        }
+
+        private static void Ser_CliClosed(object sender, CDataEventArgs<int, CTcpClientCloseArgs> e)
+        {
+            Console.WriteLine("cli {0} closed", e.Arg1);
+        }
+
+        private static void Ser_CliConnected(object sender, CDataEventArgs<int> e)
+        {
+            Console.WriteLine(e.Arg1);
+        }
+
+        private static void Ser_Closed(object sender, CDataEventArgs<CTcpServerCloseArgs> e)
+        {
+            Console.WriteLine(e.Arg1.Reason);
+        }
+    }
+}
