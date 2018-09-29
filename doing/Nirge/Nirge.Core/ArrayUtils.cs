eb5c0e3daf2a74f4a0247748d4b8f548d923a91d
nirgedeng <43745594@qq.com>
2018-09-29 13:42:16 +0800

    1

---
+++ doing/Nirge/Nirge.Core/ArrayUtils.cs
@@ -0,0 +1,39 @@
+﻿/*------------------------------------------------------------------
+    Copyright © : All rights reserved
+    Author      : 邓晓峰
+------------------------------------------------------------------*/
+
+using System.Collections.Generic;
+using System;
+
+namespace Nirge.Core
+{
+    public static class ArrayUtils
+    {
+        public static eTcpError Copy(byte[] src, int srcOffset, byte[] dst, int dstOffset, int count)
+        {
+            if (src == null)
+                return eTcpError.BlockNull;
+            if (srcOffset < 0)
+                return eTcpError.BlockSizeOutOfRange;
+            if (dst == null)
+                return eTcpError.BlockNull;
+            if (dstOffset < 0)
+                return eTcpError.BlockSizeOutOfRange;
+            if (count < 0)
+                return eTcpError.BlockSizeOutOfRange;
+
+            if (count == 0)
+                return eTcpError.Success;
+            else if (count > 8)
+                Buffer.BlockCopy(src, srcOffset, dst, dstOffset, count);
+            else
+            {
+                for (var i = 0; i < count; ++i)
+                    dst[dstOffset + i] = src[srcOffset + i];
+            }
+
+            return eTcpError.Success;
+        }
+    }
+}
