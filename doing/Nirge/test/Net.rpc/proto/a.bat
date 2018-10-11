be80630d1820a6c3671ad28ae34cc981d7958e2f
nirgedeng <43745594@qq.com>
2018-10-11 20:31:34 +0800

    1

--- doing/Nirge/test/Net.rpc/proto/a.bat
+++ doing/Nirge/test/Net.rpc/proto/a.bat
@@ -1,1 +1,1 @@
-protoc.exe --csharp_out=. --proto_path="D:\Program Files\protobuf-3.1.0\include" --proto_path="..\..\..\Nirge.Core\Rpc" a.proto
+..\..\..\deps\tools\protoc.exe --csharp_out=. --proto_path="D:\Program Files\protobuf-3.1.0\include" --proto_path="..\..\..\Nirge.Core\Rpc" --proto_path=. a.proto
